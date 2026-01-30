28809cd73f375e4fef68d9ee87ebf7911bdc8e15
pisisk <141149307+pisisk@users.noreply.github.com>
2026-01-30 23:40:46 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'guichu-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './face.jpg',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
